1199f59809456b5698986c7e12a169b704fb6519
hiroto302 <i.am.a.hiroto@gmail.com>
2021-04-21 01:24:33 +0900

    Update SpawnManager.cs
    InvokeRepeating

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -7,23 +7,24 @@ public class SpawnManager : MonoBehaviour
     int _spawnRangeX = 20;
     int _spawnRangeZ = 20;
     public GameObject[] animals = new GameObject[3];
+
+    float _startDelay = 2.0f;
+    float _spawnIntervel = 1.0f;
     void Start()
     {
-        
+        // Invokes the method methodName in time seconds, then repeatedly every repeatRate seconds.
+        InvokeRepeating("SpawnAnimal", _startDelay, _spawnIntervel);
     }
 
     void Update()
     {
-        SpawnAnimal();
+        
     }
 
     void SpawnAnimal()
     {
-        if(Input.GetKeyDown(KeyCode.S))
-        {
-            Vector3 spawnPos = new Vector3(Random.Range(-_spawnRangeX, _spawnRangeX), 0, _spawnRangeZ);
-            int randomIndex = Random.Range(0,animals.Length);
-            Instantiate(animals[randomIndex], spawnPos, animals[randomIndex].transform.rotation);
-        }
+        Vector3 spawnPos = new Vector3(Random.Range(-_spawnRangeX, _spawnRangeX), 0, _spawnRangeZ);
+        int randomIndex = Random.Range(0,animals.Length);
+        Instantiate(animals[randomIndex], spawnPos, animals[randomIndex].transform.rotation);
     }
 }
