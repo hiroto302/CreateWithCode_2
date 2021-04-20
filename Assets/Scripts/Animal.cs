ea0c32d9f1bbb34d474606286abdd25f88cae471
hiroto302 <i.am.a.hiroto@gmail.com>
2021-04-21 01:24:08 +0900

    Update Animal.cs

--- Assets/Scripts/Animal.cs
+++ Assets/Scripts/Animal.cs
@@ -7,13 +7,7 @@ public class Animal : MonoBehaviour
     [SerializeField]
     float _speed = 10.0f;
     float _lowBound = -10.0f;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
     void Update()
     {
         MoveForward();
