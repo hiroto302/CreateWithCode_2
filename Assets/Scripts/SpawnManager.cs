e674d878cad52831e8a04a64b471f3079802e3c3
hiroto302 <i.am.a.hiroto@gmail.com>
2021-04-21 00:59:11 +0900

    Create SpawManager.cs

---
+++ Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    int _spawnRangeX = 20;
+    int _spawnRangeZ = 20;
+    public GameObject[] animals = new GameObject[3];
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        SpawnAnimal();
+    }
+
+    void SpawnAnimal()
+    {
+        if(Input.GetKeyDown(KeyCode.S))
+        {
+            Vector3 spawnPos = new Vector3(Random.Range(-_spawnRangeX, _spawnRangeX), 0, _spawnRangeZ);
+            int randomIndex = Random.Range(0,animals.Length);
+            Instantiate(animals[randomIndex], spawnPos, animals[randomIndex].transform.rotation);
+        }
+    }
+}
