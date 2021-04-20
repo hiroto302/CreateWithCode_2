a1195568ec9f646b2519500e2b1abc590e7470ff
hiroto302 <i.am.a.hiroto@gmail.com>
2021-04-21 00:35:40 +0900

    Create Food.cs

---
+++ Assets/Scripts/Food.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Food : MonoBehaviour
+{
+    public float speed = 40.0f;
+
+    float _topBound = 30.0f;
+
+    void Update()
+    {
+        MoveForward();
+        DestroyOutOfBounds();
+    }
+
+    void MoveForward()
+    {
+        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+    }
+
+    void DestroyOutOfBounds()
+    {
+        if(transform.position.z > _topBound)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
